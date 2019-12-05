d66666bdb66920db22bda57f898373b721fb3e30
tawfiq <muhammedi@student.unimelb.edu.au>
2019-12-05 17:00:26 +1100

    2nd commit

---
+++ src/dqn_tfagent.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import PIL.Image
+import pyvirtualdisplay
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+tf.compat.v1.enable_v2_behavior()
+
+# Set up a virtual display for rendering OpenAI gym environments.
+display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
